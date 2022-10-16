27d898488108b00ed8ed4caa2c3c557273dd722b
triciachow0212 <suyeechow0212@gmail.com>
2022-10-16 17:58:26 +0800

    Style hero section

--- src/components/Main.server.jsx
+++ src/components/Main.server.jsx
@@ -1,14 +1,10 @@
 import { Link, Image } from "@shopify/hydrogen";
+import heroImg from "../assets/hero.jpeg";
 
 export default function Main() {
   return (
-    <section className="w-full gap-4 md:gap-8 grid p-6 md:p-8 lg:p-12">
-      <div className="whitespace-pre-wrap max-w-prose font-bold text-lead">
-        Main component
-      </div>
-      <div className="grid-flow-row grid gap-2 gap-y-6 md:gap-4 lg:gap-6 grid-cols-1 false  sm:grid-cols-3 false false">
-        Main content
-      </div>
+    <section className="w-full">
+      <Image src={heroImg} width="100%" height="100%" />
     </section>
   );
 }
